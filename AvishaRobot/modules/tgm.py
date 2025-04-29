bcf30edc8464c7b62d34bbb0a1082c99f893b606
ppppprivate <xdd28209@gmail.com>
2025-04-29 11:57:25 +0530

    Update and rename tgm.py# to tgm.py

--- AvishaRobot/modules/tgm.py
+++ AvishaRobot/modules/tgm.py
@@ -4,7 +4,7 @@ from AvishaRobot import pbot as app
 from pyrogram.types import InputMediaPhoto
 
 
-@app.on_message(filters.command(["tgm"]))
+@app.on_message(filters.command(["mmmtgm"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
@@ -18,7 +18,7 @@ def ul(_, message):
 
 ##############
 
-@app.on_message(filters.command(["graph"]))
+@app.on_message(filters.command(["mmgraph"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
